5abd7d15e1fb389b5ca0128547ed81b8191ec10f
siddharthsham <siddhsham@gmail.com>
2020-02-27 22:24:27 +0530

    Add script to build tsc files

--- blockchain.js
+++ blockchain.js
@@ -1,7 +1,10 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 exports.__esModule = true;
-var sha256_1 = require("crypto-js/sha256");
-var crypto_js_1 = require("crypto-js");
+var sha256_1 = __importDefault(require("crypto-js/sha256"));
+var crypto_js_1 = __importDefault(require("crypto-js"));
 var GenesisHash = "0x0000000000000000000000000000000000000000000000000000000000000000";
 var BlockChainDSC = {
     blockTransactLimit: 20,
